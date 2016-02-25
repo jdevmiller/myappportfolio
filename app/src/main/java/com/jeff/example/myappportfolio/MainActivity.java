e36a130f9010d2d5709b655665fb18985fb32048
jdevmiller <jdevmiller@gmail.com>
2016-02-24 19:00:00 -0500

    add onclick methods and populate them with toast messages

--- app/src/main/java/com/jeff/example/myappportfolio/MainActivity.java
+++ app/src/main/java/com/jeff/example/myappportfolio/MainActivity.java
@@ -1,9 +1,11 @@
 package com.jeff.example.myappportfolio;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,5 +35,40 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    //Launch my spotify app
+    public void spotifyButton(View view) {
+        displayToast(getString(R.string.spotify_toast));
+    }
+
+    //Launch my scores app
+    public void scoresButton(View view) {
+        displayToast(getString(R.string.scores_toast));
+    }
+
+    //Launch my libarary app
+    public void libraryButton(View view) {
+        displayToast(getString(R.string.library_toast));
+    }
+
+    //Launch my Build it Bigger app
+    public void buildBiggerButton(View view) {
+        displayToast(getString(R.string.build_bigger_toast));
+    }
+
+    //Launch my XYZ reader app
+    public void xyzButton(View view) {
+        displayToast(getString(R.string.xyz_reader_toast));
+    }
+
+    //Launch my capstone app
+    public void capstoneButton(View view) {
+        displayToast(getString(R.string.capstone_toast));
+    }
+
+    private void displayToast(String string) {
+        Toast.makeText(getApplicationContext(), string, Toast.LENGTH_SHORT).show();
     }
 }
